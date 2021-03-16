1d3615b48914bb363c74caca0cf91f46bf4ba915
john-alp <john-alp@mail.ru>
2021-03-16 10:20:48 +0300

    ant compile

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+public class HelloWorld extends JFrame {
+    public static void main(String[] args) {
+        new HelloWorld();
+    }
+    HelloWorld() {
+        setTitle(sayHello());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(200, 200, 300, 200);
+        JLabel label = new JLabel(sayHello(), SwingConstants.CENTER);
+        label.setFont(new Font("", Font.BOLD, 24));
+        add(label);
+        setVisible(true);
+    }
+    String sayHello() {
+        return "Hi world";
+    }
+}
